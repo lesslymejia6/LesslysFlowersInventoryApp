037b7deb61ab327878885b243da49fe357e5120c
lesslymejia <111783028+lesslymejia@users.noreply.github.com>
2023-07-11 12:05:13 -0500

    added InvoiceProducts model and edited Invoice model

--- FlowersInventory/InvoiceImport/models.py
+++ FlowersInventory/InvoiceImport/models.py
@@ -7,24 +7,25 @@ UNIT_TYPE_CHOICES = (
 
 
 class Invoice(models.Model):
-    product = models.ForeignKey("InvoiceImport.Product", on_delete=models.CASCADE)
-    purchase_date = models.DateTimeField(auto_now=False, auto_now_add=False)
-    unit_type = models.CharField(max_length=50, default='bunch', choices=UNIT_TYPE_CHOICES)
-    total_units = models.IntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    products = models.ManyToManyField("InvoiceImport.Product", through="InvoiceProducts")
+    purchase_date = models.DateField(auto_now=False, auto_now_add=False)
     total = models.DecimalField(max_digits=5, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.product.name
 
 
 class Product(models.Model):
     name = models.CharField(max_length=300, unique=True)
+    unit_type = models.CharField(max_length=50, default='bunch', choices=UNIT_TYPE_CHOICES)
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
+
+
+class InvoiceProducts(models.Model):
+    product = models.ForeignKey("InvoiceImport.Product", on_delete=models.CASCADE)
+    invoice = models.ForeignKey("InvoiceImport.Invoice", on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
 
 class Inventory(models.Model):
