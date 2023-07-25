ec7ef3dcc411df6dbd6b78720fb9aa16b32e33f1
lesslymejia <111783028+lesslymejia@users.noreply.github.com>
2023-07-25 14:45:28 -0500

    added jquery

--- FlowersInventory/InvoiceImport/templates/base/javaScript/invoice_view.js
+++ FlowersInventory/InvoiceImport/templates/base/javaScript/invoice_view.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
     // pulling form in
     var invoiceForm = $('.invoice-form')
-
-
+    console.log('inside .ready')
 
     invoiceForm.submit(function (event) {
-
+        console.log('inside submit function')
         // grabbing data related to this form
         var thisForm = $(this)
         // the url
