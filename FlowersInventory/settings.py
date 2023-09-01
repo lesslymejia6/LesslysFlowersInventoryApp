dfef69f5700d3049333aee1c35cfaf4be97037a3
lesslymejia <111783028+lesslymejia@users.noreply.github.com>
2023-09-01 13:31:41 -0500

    ready to deploy

--- FlowersInventory/settings.py
+++ FlowersInventory/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +37,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'InvoiceImport.apps.RenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,8 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Our Apps
-    'InvoiceImport',
 ]
 
 MIDDLEWARE = [
